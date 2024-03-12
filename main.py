5c2391a8c648cd5452c14a08401796a2b8d8af4b
srivastavajaideep <srivastava.jaideep@gmail.com>
2024-03-12 21:29:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 import nltk
 from PIL import Image
 warnings.filterwarnings('ignore')
-
+nltk.download('punkt')
 
 # st.cache_data.clear()
 st.set_page_config(page_title="WU App Review DashBoard!!!", page_icon=":sparkles:",layout="wide")
