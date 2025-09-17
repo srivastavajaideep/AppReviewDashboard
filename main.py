6ac8ef5f7cf8713f55b65eaa724ecc5449f76b2e
srivastavajaideep <srivastava.jaideep@gmail.com>
2025-09-17 14:38:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1607,11 +1607,12 @@ if not filtered_df.empty:
 
  
 
-    st.dataframe(filtered_df,column_config=column_config, height=275, use_container_width=True)
+  st.text("")  # Adds one empty line of vertical space
+ 
+  st.dataframe(filtered_df, column_config=column_config, height=275, use_container_width=True)
+  st.success(f"✅ Displaying {len(filtered_df)} reviews.")
+  st.markdown("<br>", unsafe_allow_html=True)
 
-    st.success(f"✅ Displaying {len(filtered_df)} reviews.")
-
-    st.markdown("<br>", unsafe_allow_html=True)
 
  
 
@@ -3100,6 +3101,7 @@ st.markdown(f"""
 
 
 """, unsafe_allow_html=True)
+
 
 
 
